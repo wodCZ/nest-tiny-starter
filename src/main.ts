e060ede14d19fdf1b478da56fa91bb3decd6cf5e
Martin Janeček <admin@ikw.cz>
2023-11-04 14:43:38 +0100

    feat: add prisma

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import { NestFactory } from "@nestjs/core";
-import { Controller, Get, Module } from "@nestjs/common";
+import { Controller, Get, Injectable, Module } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+class PrismaService extends PrismaClient {}
 
 @Controller()
-class StatusController {
-  @Get("/status")
-  status() {
-    return {
-      status: "ok",
-    };
+class PostsController {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Get("/posts")
+  findPosts() {
+    return this.prismaService.post.findMany();
   }
 }
 
-@Module({ controllers: [StatusController] })
+@Module({
+  controllers: [PostsController],
+  providers: [PrismaService],
+})
 class AppModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 
